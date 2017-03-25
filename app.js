6e79d06f7eda78a7255cfe5d2f0a4ff3dbd4f804
tiagofoks <tiagofoks@yahoo.com.br>
2017-03-25 19:38:47 -0300

    Visual-recognition code add

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ var validator = require('validator');
 
 var photoCollections = require('./collections/photoCollections.js');
 
+var watson = require('watson-developer-cloud');
+var fs = require('fs');
+
+var visual_recognition = watson.visual_recognition({
+  api_key: '{api_key}',
+  version: 'v3',
+  version_date: '2016-05-20'
+});
+
+var params = {
+  images_file: fs.createReadStream('./resources/car.png')
+};
+
+visual_recognition.classify(params, function(err, res) {
+  if (err)
+    console.log(err);
+  else
+    console.log(JSON.stringify(res, null, 2));
+});
+
 app.get('/', function(req, res){
 	
 	res.end('Servidor ON!');
